eada7100e86f90aa4006d4013cf58e7c9379783e
Hassna16 <hassna.agouzzal@edu.ece.fr>
2022-11-15 12:03:15 +0100

    AJOUT DES FICHIERS timer

---
+++ frontend/src/Timer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '.\Timer.css';
+
+const Timer = () => {
+
+    return (
+        <div className="Timer">
+            <div className="container">
+                <div className="timer_container">
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+
+
+
+}
